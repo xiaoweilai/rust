e08178a9f5e93d978fe27737b9925d8f5adb8fd3
yjduoduo <wxjlmr@126.com>
2015-07-02 22:30:33 +0800

    eat

--- Dining_Philosophers/src/main.rs
+++ Dining_Philosophers/src/main.rs
@@ -1,21 +1,29 @@
 struct Philosopher {
     name: String,
-}
+}   
 
-impl Philosopher {
+impl Philosopher { 
     fn new(name: &str) -> Philosopher {
         Philosopher {
             name: name.to_string(),
         }
     }
+    
+    fn eat(&self) {
+        println!("{} is done eating.", self.name);
+    }
 }
 
-
-
 fn main() {
-    let p1 = Philosopher { name: "Baruch Spinoza".to_string() };
-    let p2 = Philosopher { name: "Gilles Deleuze".to_string() };
-    let p3 = Philosopher { name: "Karl Marx".to_string() };
-    let p4 = Philosopher { name: "Friedrich Nietzche".to_string() };
-    let p5 = Philosopher { name: "Michel Foucault".to_string() };
+    let philosophers = vec![
+        Philosopher::new("Baruch Spinoza"),
+        Philosopher::new("Gilles Deleuze"),
+        Philosopher::new("Karl Marx"),
+        Philosopher::new("Friedrich Nietzsche"),
+        Philosopher::new("Michel Foucault"),
+    ];
+
+    for p in &philosophers {
+        p.eat();
+    }
 }
