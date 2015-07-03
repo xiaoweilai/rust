08abb49e2c06df42844a68c9a48b54e101cc63e1
yjduoduo <wxjlmr@126.com>
2015-07-04 01:58:14 +0800

    Start ten threads. Inside each thread, count from one to five million. After All ten threads are finished, print out ‘done!’.

---
+++ five_million/src/main.rs
@@ -0,0 +1,15 @@
+use std::thread;
+threads = []
+
+10.times do
+  threads << Thread.new do
+    count = 0
+
+    5_000_000.times do
+      count += 1
+    end
+  end
+end
+
+threads.each {|t| t.join }
+puts "done!"
